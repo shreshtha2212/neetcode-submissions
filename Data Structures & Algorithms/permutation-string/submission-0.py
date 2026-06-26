41c655d0a09479e882af3125c575d3fb23b1a304
shreshtha2212 <104908363+shreshtha2212@users.noreply.github.com>
2026-06-26 11:58:20 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1)>len(s2):
+            return False
+        freq1=[0]*26
+        freq2=[0]*26
+        for i in range(len(s1)):
+            freq1[ord(s1[i])-ord('a')]+=1
+        window=len(s1)
+        for i in range(window):
+            freq2[ord(s2[i])-ord('a')]+=1
+        if freq1==freq2:
+            return True
+        
+        for i in range(window,len(s2)):
+            freq2[ord(s2[i])-ord('a')]+=1
+            freq2[ord(s2[i-window])-ord('a')]-=1
+            if freq1==freq2:
+                return True
+        return False
+
+
+
+
+        

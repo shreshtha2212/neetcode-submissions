34ab381b155747375bbd1f2b181f0e7b802e145e
shreshtha2212 <104908363+shreshtha2212@users.noreply.github.com>
2026-07-29 11:11:31 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def getPerm(i,arr,ans):
+            if i==len(nums):
+                ans.append(arr.copy())
+                return
+            for idx in range(i,len(arr)):
+                arr[i],arr[idx]=arr[idx],arr[i]
+                getPerm(i+1,arr,ans)
+                arr[idx],arr[i]=arr[i],arr[idx]
+        ans=[]
+        getPerm(0,nums,ans)
+        return ans
+
+        
